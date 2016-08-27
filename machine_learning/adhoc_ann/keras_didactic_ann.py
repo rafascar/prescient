87a227bcca2a765b45c1fca9200fd09f83e06a56
rsmeurer0 <rsmeurer0@gmail.com>
2016-08-26 21:21:48 -0300

    Changes to keras script to test with deeper nets

--- machine_learning/adhoc_ann/keras_didactic_ann.py
+++ machine_learning/adhoc_ann/keras_didactic_ann.py
@@ -23,14 +23,15 @@ model.add(Dense(1, init='normal'))
 sgd = SGD(lr=0.001, decay=0, momentum=0.0, nesterov=False)
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X,y,nb_epoch=15000, batch_size=5, verbose=1, validation_data=(X,y))
+model.fit(X,y,nb_epoch=1500, batch_size=5, verbose=0, validation_data=(X,y))
 
 scores = model.evaluate(X,y)
 print(scores)
 print("\n\n%s: %.10f%%" % (model.metrics_names[1], scores[1]*100))
 print (model.metrics_names)
+print (scores[0]*100, scores[1]*100)
 
 result = model.predict(X)
 print (result.shape)
 
-print (y - model.predict(X))
+print (np.rint(y - model.predict(X)))
