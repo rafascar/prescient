cc38f93131e4bf01f85e65a9b39b89ab56404fde
Rodrigo Schmitt Meurer <rsmeurer0@gmail.com>
2016-09-27 14:32:34 -0300

    Included class to deal with photo resistor.
    ATTENTION: The value in percentage is directly associeted with the electronic connection in the galileo board. Make sure that you use the right one.

---
+++ gateway/sensors/luminosity/photo_resistor.py
@@ -0,0 +1,19 @@
+from wiringx86 import GPIOGalileoGen2 as GPIO
+
+class photo_resistor(object):
+    """
+    Instatiates a analog read of the GPIO pin from galileo gen2 
+
+    pin: Valid Analog Pin (14, 15, 16, 17, 18, 19) respectively from A0 to A5
+    """
+    # Class Initializer
+    def __init__(self, pin, debug=False):
+        self.gpio = GPIO(debug=False)
+        self.pin = pin
+        self.gpio.pinMode(self.pin, self.gpio.ANALOG_INPUT)
+
+    def get_value(self):
+        self.raw = self.gpio.analogRead(self.pin)
+        self.normalized = (self.raw/1024.0)
+        self.percent = 100 - self.normalized * 100
+        return self.percent 
