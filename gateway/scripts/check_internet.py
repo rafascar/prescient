5a6eeb91959e474cf09f774179c7cd5f68d720fd
Rodrigo Schmitt Meurer <rsmeurer0@gmail.com>
2016-09-29 15:12:43 -0300

    Corrected thread segmentation fault bug

--- gateway/scripts/check_internet.py
+++ gateway/scripts/check_internet.py
@@ -3,14 +3,22 @@
 import os
 import time
 import threading
-#from wiringx86 import GPIOGalileoGen2 as GPIO
+from wiringx86 import GPIOGalileoGen2 as GPIO
 
-#gpio = GPIO(debug=False)
+gpio = GPIO(debug=False)
+
+internet_pin = 1
+gpio.pinMode(internet_pin, gpio.OUTPUT)
+
 
 def check_internet():
-    threading.Timer(3, check_internet).start()
     response = os.system("ping -c 1 google.com -q > /dev/null 2>&1") # ping 0 = success
     if response == 0:
         print ("Connected")
+        gpio.digitalWrite(internet_pin, gpio.HIGH)
+        threading.Timer(3, check_internet).start()
+
     else: 
         print("Fuck")
+        gpio.digitalWrite(internet_pin, gpio.LOW)
+        threading.Timer(3, check_internet).start()
