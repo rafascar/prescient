d1396a93a3c9e6dddf38be7673f1fd38544ce23a
Rodrigo Schmitt Meurer <rsmeurer0@gmail.com>
2016-09-30 21:17:48 -0300

    Detect user is now using path as argument fot the user class constructor

--- gateway/scripts/detect_user.py
+++ gateway/scripts/detect_user.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+import sys
+sys.path.append("../gpio")
+
 import subprocess
 import threading
 import time
@@ -11,8 +14,9 @@ user_pin = 0
 gpio.pinMode(user_pin, gpio.OUTPUT)
 
 class user(object):
-    def __init__(self, MAC, name):
+    def __init__(self, path, MAC, name):
         self.connected = False
+        self.scripts_path = path
         self.last_connected = 0
         self.name = name
         self.mac_addr = MAC
@@ -26,7 +30,7 @@ class user(object):
             return False
 
     def search_net(self):
-        self.ip = subprocess.check_output("./find_mac.sh %s" %(self.mac_addr), shell=True)
+        self.ip = subprocess.check_output("%sfind_mac.sh %s" %(self.scripts_path,self.mac_addr), shell=True)
         self. ip = self.ip[:-1].decode("utf-8")
         valid = self.validate_ip()
         if valid:
@@ -41,7 +45,8 @@ def find_user(smartphone):
    # if the smartphone is currently with status connected False keep searching the net for it
    if not smartphone.connected:
        smartphone.search_net()
-       threading.Timer(0.5, find_user, [smartphone]).start()
+       threading.Timer(5, find_user, [smartphone]).start()
+       return
    else:
    # if the smartphone was found keep cheking it's IP address to verify if it is still connected
        response = os.system("ping -c 1 " + smartphone.ip) # ping 0 = success
@@ -51,16 +56,19 @@ def find_user(smartphone):
            smartphone.last_connected = time.time()
        if response != 0 and (time.time() - smartphone.last_connected > 300):
            smartphone.connected = False # if smartphone disconnected change status to begin searching for it again
-           threading.Timer(0.5, find_user, [smartphone]).start()
+           threading.Timer(5, find_user, [smartphone]).start()
            gpio.digitalWrite(user_pin, gpio.LOW)
+           return
        else:
            print(time.time() - smartphone.last_connected)
            #time.sleep(5)
            threading.Timer(5, find_user, [smartphone]).start()
+           return
 
 if __name__ == '__main__':
+    scripts_path = './'
     with open('users.txt') as users:
         for line in users:
             column = line.split()
-            smartphone = user(column[0], column[1])
+            smartphone = user(scripts_path, column[0], column[1])
     find_user(smartphone)
