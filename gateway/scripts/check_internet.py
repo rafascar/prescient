60b28d74b4a754a987f984c971c5cba4999a7620
Rodrigo Schmitt Meurer <rsmeurer0@gmail.com>
2016-10-05 14:02:00 -0300

    Remove threading in the scripts to try new approach and solve segmentation fault error.

--- gateway/scripts/check_internet.py
+++ gateway/scripts/check_internet.py
@@ -9,7 +9,6 @@ import threading
 from wiringx86 import GPIOGalileoGen2 as GPIO
 
 gpio = GPIO(debug=False)
-
 internet_pin = 1
 gpio.pinMode(internet_pin, gpio.OUTPUT)
 
@@ -17,13 +16,13 @@ gpio.pinMode(internet_pin, gpio.OUTPUT)
 def check_internet():
     response = os.system("ping -c 1 google.com -q > /dev/null 2>&1") # ping 0 = success
     if response == 0:
-        print ("Connected to internet")
+        print ("CONNECTIVITY: \tConnected to internet")
         gpio.digitalWrite(internet_pin, gpio.HIGH)
-        threading.Timer(30, check_internet).start()
+        #threading.Timer(30, check_internet).start()
         return
 
     else: 
-        print("Unable to connect to internet")
+        print("CONNECTIVITY: \tUnable to connect to internet")
         gpio.digitalWrite(internet_pin, gpio.LOW)
-        threading.Timer(30, check_internet).start()
+        #threading.Timer(30, check_internet).start()
         return
